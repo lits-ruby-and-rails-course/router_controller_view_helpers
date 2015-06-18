4d89fd16a6261096e39cc7686f4f8c5361c5eeec
Anton <anton.perebyinis@gmail.com>
2015-06-18 15:05:44 +0300

    my solution

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,12 +5,17 @@ class LikesController < ApplicationController
   # POST /likes.json
   def create
     @parrent.add_like!
-    redirect_to :back
+    redirect_to :back, notice: "Thank you for your like!"
   end
 
   private
 
     def get_parrent
-      @parrent = nil
+      params.each do |name, value|
+        if name =~ /(.+)_id$/
+          @parrent = $1.classify.constantize.find(value)
+        end
+      end
+      nil
     end
 end
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,10 @@
 module LikesHelper
+  def count_likes object
+    object.likes.count
+  end
+  def count_likes_for_collection collection
+    @counter = 0
+    collection.map {|obj| @counter += obj.likes.count}
+    return @counter
+  end
 end
